411ba37bf03adecf3aebabba8716adc8acfc47bd
Daka-fish <yamatohidaka24@gmail.com>
2024-03-29 14:01:14 +0900

    画像の差し替え

--- src/components/SidebarIcon.js
+++ src/components/SidebarIcon.js
@@ -1,11 +1,13 @@
 import React from "react";
-import Icon from "../images/icon.png";
+import Icon from "../images/Kyudo_back.png";
 
 function SidebarIcon() {
   return (
     <div className="SidebarIcon">
       <img src={Icon} />
-      <p>mailaddress@mail.com</p>
+      <p>
+        <a href="https://twitter.com/chrono_f1sh">X(Twitter)</a>
+      </p>
     </div>
   );
 }
